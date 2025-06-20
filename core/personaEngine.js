cd5716f22cccb773627c50f4206efd3e43e9ccb3
web <michaelebuzome4@gmail.com>
2025-06-20 07:08:09 +0100

    Update personaEngine.js

--- core/personaEngine.js
+++ core/personaEngine.js
@@ -1,1 +1,5 @@
+// core/personaEngine.js
 
+export function applyPersonaTone(persona, response) { switch (persona) { case "mentor": return 🧙 Mentor says:\n${response}; case "genz": return 💅 Gen Z Vibe:\n${response}\n#SlayOn; case "engineer": return ⚙️ Terminal Output:\n>>> ${response}; case "sage": return 🌿 From the Sage:\n"${response}"; default: return response; } }
+
+    
